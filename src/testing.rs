5a895269e91f1e44128aa22b9501aeb674fc5240
Zibi Braniecki <zibi@braniecki.net>
2020-12-31 17:09:56 -0800

    Use async file fetching for async operations

--- src/testing.rs
+++ src/testing.rs
@@ -1,6 +1,5 @@
 use crate::{FileFetcher, FileSource};
 use async_trait::async_trait;
-use fluent_testing::get_test_file;
 use unic_langid::LanguageIdentifier;
 
 pub struct TestFileFetcher;
@@ -8,12 +7,11 @@ pub struct TestFileFetcher;
 #[async_trait]
 impl FileFetcher for TestFileFetcher {
     fn fetch_sync(&self, path: &str) -> std::io::Result<String> {
-        println!("{}", path);
-        get_test_file(path)
+        fluent_testing::get_test_file_sync(path)
     }
 
     async fn fetch(&self, path: &str) -> std::io::Result<String> {
-        get_test_file(path)
+        fluent_testing::get_test_file_async(path).await
     }
 }
 
